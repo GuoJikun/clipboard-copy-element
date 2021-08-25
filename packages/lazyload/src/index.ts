caad3941e52e88105b04905ceb763908ba203e2a
GuoJikun <guojikun@y-open.com>
2021-08-25 10:33:39 +0800

    添加lerna

---
+++ packages/lazyload/src/index.ts
@@ -0,0 +1,85 @@
+import { App } from "vue";
+
+const LazyLoad = {
+  // install方法
+  install(Vue: App, options: any) {
+    const defaultSrc = options.default;
+    Vue.directive("lazy", {
+      beforeMount(el, binding) {
+        LazyLoad.init(el, binding.value, defaultSrc);
+      },
+      mounted(el) {
+        if (IntersectionObserver) {
+          LazyLoad.observe(el);
+        } else {
+          LazyLoad.listenerScroll(el);
+        }
+      },
+    });
+  },
+  // 初始化
+  init(el: HTMLElement, val: string, def: string) {
+    el.setAttribute("data-src", val);
+    el.setAttribute("src", def);
+  },
+  // 利用IntersectionObserver监听el
+  observe(el: HTMLElement) {
+    const io = new IntersectionObserver((entries) => {
+      const realSrc = el.dataset.src;
+      if (entries[0].isIntersecting) {
+        if (realSrc) {
+          el.setAttribute("src", realSrc);
+          el.removeAttribute("data-src");
+        }
+      }
+    });
+    io.observe(el);
+  },
+  // 监听scroll事件
+  listenerScroll(el: HTMLElement) {
+    const handler = LazyLoad.throttle(LazyLoad.load, 300);
+    LazyLoad.load(el);
+    window.addEventListener("scroll", () => {
+      handler(el);
+    });
+  },
+  // 加载真实图片
+  load(el: HTMLElement) {
+    const windowHeight = document.documentElement.clientHeight;
+    const elTop = el.getBoundingClientRect().top;
+    const elBtm = el.getBoundingClientRect().bottom;
+    const realSrc = el.dataset.src;
+    if (elTop - windowHeight < 0 && elBtm > 0) {
+      if (realSrc) {
+        el.setAttribute("src", realSrc);
+        el.removeAttribute("data-src");
+      }
+    }
+  },
+  // 节流
+  throttle(fn: Function, delay: number) {
+    let timer: number;
+    let prevTime;
+    return function (...args) {
+      const currTime = Date.now();
+      const context = this;
+      if (!prevTime) prevTime = currTime;
+      clearTimeout(timer);
+
+      if (currTime - prevTime > delay) {
+        prevTime = currTime;
+        fn.apply(context, args);
+        clearTimeout(timer);
+        return;
+      }
+
+      timer = setTimeout(function () {
+        prevTime = Date.now();
+        timer = null;
+        fn.apply(context, args);
+      }, delay);
+    };
+  },
+};
+
+export default LazyLoad;
--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <div>
-    <label for="test" v-copy>test copy</label>
-    <input type="text" value="123" id="test" />
-  </div>
-  <div>
-    <p v-copy @clipboard-copy="test" value="eeee">测试复制指令</p>
-  </div>
-  <div>
-    <input v-copy value="测试复制指333令" />
+    <div>
+      <h2>用法一</h2>
+      <h1>
+        元素本身有value属性(可以通过getAttriubte方法获取)则直接复制value属性的值
+      </h1>
+      <div>
+        <button v-copy @clipboard-copy="test" value="这是通过value属性复制的值">
+          通过value属性复制
+        </button>
+      </div>
+    </div>
+    <div>
+      <h2>用法二</h2>
+      <h1>对于 a 标签则直接复制 href 属性的值</h1>
+      <div>
+        <a v-copy @clipboard-copy="test" href="https://www.baidu.com">
+          a 标签则复制 href 属性
+        </a>
+      </div>
+    </div>
+    <div>
+      <h2>用法三</h2>
+      <h1>对于 input 标签则是绑定指令到其他标签然后通过其 for 属性去复制</h1>
+      <div>
+        <label for="test" v-copy>测试input元素的复制</label>
+        <input type="text" value="这是input元素value的值" id="test" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 import { Copy } from "./components/clipboard-copy-element/index";
 
 export default defineComponent({
   name: "App",
   directives: { Copy },
-  components: {
-    HelloWorld,
-  },
-  methods: {
-    test() {
+  setup() {
+    const test = () => {
       alert(1);
-    },
+    };
+    return { test };
   },
 });
 </script>
